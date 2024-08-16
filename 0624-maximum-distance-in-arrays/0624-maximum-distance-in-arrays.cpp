fddaafb93762f152d09c3bb95e1d3d5ff2e55cbc
Shubhay Gautam <shubhaygautam@gmail.com>
2024-08-16 22:44:52 +0530

    Time: 155 ms (100.00%), Space: 108.1 MB (26.41%) - LeetHub

---
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& arrays) {
+        if (arrays.size() < 2) return 0;
+
+        int globalMin = arrays[0][0];
+        int globalMax = arrays[0].back();
+        int result = 0;
+
+        for (int i = 1; i < arrays.size(); ++i) {
+ 
+            int localMin = arrays[i][0];
+            int localMax = arrays[i].back();
+
+          
+            result = max(result, max(localMax - globalMin, globalMax - localMin));
+
+            globalMin = min(globalMin, localMin);
+            globalMax = max(globalMax, localMax);
+        }
+
+        return result;
+    }
+};
+
+static const int speedup = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
