52080e05baa9ce8252c763eafd5c189a34a64fd4
Shubhay Gautam <shubhaygautam@gmail.com>
2024-09-04 23:00:18 +0530

    Time: 59 ms (88.12%), Space: 36 MB (99.45%) - LeetHub

---
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    static int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        const long long M=60001, lb=-30000;
+        unordered_set<long long> obSet;
+        obSet.reserve(obstacles.size());
+        for(auto& ob: obstacles){
+            long long x=ob[0]-lb, y=ob[1]-lb;
+            obSet.insert(x*M+y);
+        }
+
+        const int dir[4][2]={{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
+        int x=0, y=0, dx=0, dy=1, face=0, maxD2=0;
+        for(int c: commands){
+            switch(c){
+                case -2: face=(face+1)%4; dx=dir[face][0]; dy=dir[face][1]; break;
+                case -1: face=(face+3)%4; dx=dir[face][0]; dy=dir[face][1]; break;
+                default:
+                //    cout<<"\nc="<<c<<" face="<<face<<":";
+                    for(int i=0; i<c; i++){
+                        x+=dx, y+=dy;
+                        if (obSet.count((x-lb)*M+y-lb)) {
+                            x-=dx;  // previous move
+                            y-=dy;
+                            break;
+                        }
+                    //    cout<<"("<<x<<","<<y<<"),";
+                        maxD2=max(maxD2, x*x+y*y);
+                    }
+            }
+        }
+        return maxD2;
+    }
+};
