11a2738f482d62e748f553dd3a812f821e97de8f
Shubhay Gautam <shubhaygautam@gmail.com>
2025-09-02 19:37:21 +0530

    Time: 0 ms (100%), Space: 15.1 MB (94.71%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,40 +1,29 @@
 class Solution {
 public:
-    int binaryS(vector<int>& nums, int target, int l, int r){
+    int search(vector<int>& nums, int target) {
+        int n = nums.size(), l = 0, r = n-1;
         while(l<=r){
-            int mid = l+(r-l)/2;
-            if(nums[mid] == target){
-                return mid;
+            int mid = l + (r-l) / 2;
+
+            if(nums[mid] == target) return mid;
+
+            // Check if left half is sorted
+        if (nums[l] <= nums[mid]) {
+            if (nums[l] <= target && target < nums[mid]) {
+                r = mid - 1; // target lies in left half
+            } else {
+                l = mid + 1; // target lies in right half
             }
-            if(target > nums[mid]){
-                l = mid+1;
+        }
+        // Otherwise, right half is sorted
+        else {
+            if (nums[mid] < target && target <= nums[r]) {
+                l = mid + 1; // target lies in right half
+            } else {
+                r = mid - 1; // target lies in left half
             }
-            else{
-                r = mid-1;
-            }
+        }
         }
         return -1;
-    }
-    int search(vector<int>& nums, int target) {
-        int n = nums.size(), pivot = 0;
-        int l = 0, r = n-1;
-        
-        while(l<=r){
-            int mid = l + (r-l)/2;
-            if(l == r){
-             pivot = l;
-            }
-            if(nums[mid]<nums[r]){
-                r = mid;
-            }
-            else{
-                l += 1;
-            }
-        }
-        if (target >= nums[pivot] && target <= nums[n - 1]) {
-            return binaryS(nums, target, pivot, n - 1);
-        } else {
-            return binaryS(nums, target, 0, pivot - 1);
-        }
     }
 };
